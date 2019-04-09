c14e3afbbbf9b1ad52db6259cfc4ce5329f00d6f
BreadBug007 <47244587+BreadBug007@users.noreply.github.com>
2019-04-10 00:31:50 +0530

    Add files via upload

--- Novel_Updates.py
+++ Novel_Updates.py
@@ -2,16 +2,18 @@ from selenium import webdriver
 import requests
 from bs4 import BeautifulSoup as bs
 from time import sleep
-import os, sys
+import os
+import sys
 
 
 def site_login():
-    url = "https://www.novelupdates.com/login/"
-    chrome.get(url)
+    chrome.get("https://www.novelupdates.com/login/")
     sleep(3)
     try:
         chrome.find_element_by_class_name("qc-cmp-button").click()
-    except:
+    except Exception as e:
+        print("No 'Privacy and cookie policy message' ")
+        print(e)
         pass
     sleep(2)
     chrome.find_element_by_id("user_login").send_keys(username)
@@ -25,36 +27,66 @@ def find_page(first_chap, input_chap):
     return pg + 1
 
 
+def get_group(url):
+    button = chrome.find_element_by_css_selector("i.fa.fa-filter.chp")
+    chrome.execute_script("arguments[0].scrollIntoView();", button)
+    button.click()
+    sleep(2)
+
+    table = chrome.find_element_by_css_selector("ol.sp_grouptable")
+    entries = table.find_elements_by_class_name("grp-filter-attr")
+
+    page_dict = {}
+
+    for entry in entries:
+        group_id = entry.get_attribute("value")
+        try:
+            grp_url = url + "?grp={}".format(group_id)
+            with requests.Session() as s:
+                source = s.get(grp_url)
+            soup = bs(source.content, 'lxml')
+            soup = soup.find('div', class_="digg_pagination")
+            pages = soup.find_all("a")
+            page_count = int(pages[-2].text)
+            page_dict[group_id] = page_count
+        except Exception as e:
+            pass
+
+    return max(page_dict, key=page_dict.get)
+
+
 def search_novel(search_term):
     url = "https://www.novelupdates.com/?s={}&post_type=seriesplans".format(search_term)
-
     chrome.get(url)
-
     chrome.find_element_by_css_selector("span.w-blog-entry-title-h.entry-title").click()
-
     return chrome.current_url
 
 
 def edit(url, input_chap):
+    group_id = get_group(url)
+    new_url = url + "?grp={}".format(group_id)
+
     with requests.Session() as s:
-        source = s.get(url)
+        source = s.get(new_url)
 
     soup = bs(source.text, 'lxml')
-
     first_chap = soup.find('a', class_='chp-release').text
-
     search_page = find_page(first_chap, input_chap)
 
-    search_url = url + "?pg=" + str(search_page) + "&grp=23"
+    if search_page == 0:
+        print("Chapter not available")
+        return
+
+    search_url = url + "?pg=" + str(search_page) + "&grp={}".format(group_id)
 
     chrome.get(search_url)
+    sleep(2)
 
     table = chrome.find_element_by_css_selector('#myTable')
-
     chapters = table.find_elements_by_xpath('.//tr')
+
     for chapter in chapters[1:]:
         try:
-
             check_conditions = chapter.find_element_by_css_selector("label.enableread")
             chap_no = chapter.find_element_by_class_name("chp-release").text
 
@@ -63,7 +95,6 @@ def edit(url, input_chap):
                 print("Chapter found and checked")
                 sleep(5)
                 return
-
         except Exception as e:
             print(e)
 
